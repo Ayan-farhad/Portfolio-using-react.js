464d0d8ec1d9248c5363092bbbfcc4b4b8479bc8
Ayan farhad <khaanayan81@gmail.com>
2024-06-22 17:15:11 +0500

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ayan Portfolio</h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/component/Navbar/navbar.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './navbar.css';
+
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+import { AiOutlineClose } from "react-icons/ai";
+import { IoHomeOutline } from "react-icons/io5";
+import { GoPerson } from "react-icons/go";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { AiOutlineMessage } from "react-icons/ai";
+import { LiaGraduationCapSolid } from "react-icons/lia";
+
+const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className='nav-logo'>{"<"}Ayan{" />"}</div>
+
+            <div className='nav-hamburger' onClick={toggleMobileMenu}>
+                <div className={`icon-container ${isMobileMenuOpen ? 'open' : ''}`}>
+                    {isMobileMenuOpen ? <AiOutlineClose /> : <HiOutlineBars3BottomRight />}
+                </div>
+            </div>
+
+            <div className={`nav-bar ${isMobileMenuOpen ? 'open' : ''}`}>
+                <ul>
+                    <li className='nav-li'>
+                        <a href='#' className='active'><IoHomeOutline className='nav-icon'/>Home</a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='#'><GoPerson className='nav-icon'/>About</a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='#'><AiOutlineFundProjectionScreen className='nav-icon'/>Projects</a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='#'><LiaGraduationCapSolid className='nav-icon'/>Skills</a>
+                    </li>
+                    <li className='nav-li'>
+                        <a href='#'><AiOutlineMessage className='nav-icon'/>Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
